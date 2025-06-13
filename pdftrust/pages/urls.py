9d76b88bdfc45a9e5594d7a3c8c03a3e8caca169
mhostn3 <martin.hostnik@protonmail.com>
2025-06-13 12:24:19 +0100

    update

--- pdftrust/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('merge/', views.merge_view, name='merge'),
-    path('split/', views.split_view, name='split'),
-]
